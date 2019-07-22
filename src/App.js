9423e893b5b5df800cd4411ce645d95b7beb7a0f
mi4en <marionikolov87@gmail.com>
2019-07-22 22:58:46 +0300

    add post and; add get;

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ import PostForm from './components/PostForm';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="App container">
 			<header className="App-header">
 				<img src={logo} className="App-logo center" alt="logo" />
 				<PostForm />
 				<hr />
 				<Post />
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
 			</header>
 		</div>
 	);
